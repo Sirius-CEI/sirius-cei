020c5a31e1b280ea1f18ba2fec9df9658e2398d5
Cooper Miller <coopermllr@gmail.com>
2018-12-15 21:05:22 -0600

    working on backend connecting card edit, delete, add

--- server/routes/cards.router.js
+++ server/routes/cards.router.js
@@ -17,27 +17,24 @@ router.get('/', (req, res) => {
   });
 
 // add a Card
-// router.post('/', (req, res) => {
-//     console.log('post info: ', req.body);
-//     const newProject = req.body;
-//     const queryText = `INSERT INTO projects ("name", "description", "thumbnail", "website", "github", "date_completed", "tag_id")
-//                     VALUES ($1, $2, $3, $4, $5, $6, $7)`;
-//     const queryValues = [
-//         newProject.name,
-//         newProject.description,
-//         newProject.thumbnail,
-//         newProject.website,
-//         newProject.github,
-//         newProject.date_completed,
-//         newProject.tag_id,
-//     ];
-//     pool.query(queryText, queryValues)
-//       .then(() => { res.sendStatus(201); })
-//       .catch((err) => {
-//         console.log('Error completing SELECT projects query', err);
-//         res.sendStatus(500);
-//       });
-//   });
+router.post('/', (req, res) => {
+    console.log('post card info: ', req.body);
+    const addCard = req.body;
+    const queryText = `INSERT INTO cards ("title", "image", "url", "category_id")
+                    VALUES ($1, $2, $3, $4)`;
+    const queryValues = [
+        addCard.title,
+        addCard.image,
+        addCard.url,
+        addCard.category_id,
+    ];
+    pool.query(queryText, queryValues)
+      .then(() => { res.sendStatus(201); })
+      .catch((error) => {
+        console.log('Error completing POST card query', error);
+        res.sendStatus(500);
+      });
+  });
 
 // // put request route to send new projects information to database
 // router.put('/', (req, res) => {
--- src/redux/sagas/cardSaga.js
+++ src/redux/sagas/cardSaga.js
@@ -10,10 +10,47 @@ function* getCards() {
     } catch (error) {
       console.log('Card get request failed', error);
     }
+}
+
+function* addCard(action) {
+    console.log('POST saga to add Card: ', action.payload);
+    try {
+        // asynch call to add card to server
+        yield call(axios.post, '/cards', action.payload);
+        yield put( { type: 'SET_CARDS' } );
+    }
+    catch (error) {
+        console.log('error with POST request to /cards', error);
+    }
+  }
+  
+  function* deleteCard(action) {
+    console.log('in delete saga to remove card: ', action.payload);
+    try {
+        // axios asynch call to add koala to server
+        yield call(axios.delete, '/api/koalas', {params: {id: action.payload}});
+        yield put( { type: 'GET_KOALAS' } );
+    }
+    catch (error) {
+        console.log('error with delete request to /api/koalas');
+    }
+  }
+  
+  function* transferKoala(action) {
+    console.log('in saga for set ready to transfer for a koala', action.payload);
+    try {
+        // axios asynch call to add koala to server
+        yield call(axios.put, '/api/koalas/transfer', action.payload);
+        yield put( { type: 'GET_KOALAS' } );
+    }
+    catch (error) {
+        console.log('error with transfer request to /api/koalas/transfer');
+    }
   }
 
 function* cardSaga() {
   yield takeEvery('GET_CARDS', getCards);
+  yield takeEvery('ADD_CARD', addCard)
 }
 
 export default cardSaga;
