907731200ec3947b81a3a9be6e52e071da435b46
phitranpt <phi.tran.pt@gmail.com>
2018-12-17 16:06:08 -0600

    added title, cards, links, and contact information to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,127 @@
 import React from 'react';
-import './Footer.css'
+import './Footer.css';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
+import 'typeface-lato';
+import propTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const Footer = () => (
-  <footer>
-    &copy; Prime Digital Academy
-  </footer>
-);
+import { Link } from 'react-router-dom';
 
-export default Footer;
+const styles = {
+  card: {
+    maxWidth: 345,
+    margin: '20px',
+    display: 'inline-block',
+    width: '100%',
+  },
+  media: {
+    height: 200,
+  },
+  title: {
+    color: 'white',
+    textAlign: 'center',
+    paddingTop: 40,
+    fontSize: 30,
+    fontWeight: 700,
+    wordWrap: 'breakWord',
+    fontFamily: 'Lato',
+    lineHeight: '1em',
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'black'
+  },
+  button: {
+    borderRadius: 50,
+    fontSize: 15,
+    fontWeight: 500,
+    marginTop: 45,
+    backgroundColor: 'white',
+    "&:hover": {
+      backgroundColor: "white"
+    }
+  }
+};
+
+const Footer = (props) => {
+
+  const { classes } = props;
+
+  return (
+    <footer>
+      <div className="footer-title">See the full impact</div>
+      <div className="cards">
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.media}
+            image="images/macro.jpeg"
+            title="macro">
+            <Typography className={classes.title} gutterBottom variant="h5" component="h2">
+              Macro
+            </Typography>
+            <Button className={classes.button}>
+              <Link className={classes.link} to="/macro">Read More</Link>
+            </Button>
+          </CardMedia>
+        </Card>
+
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.media}
+            image="images/economic-development.jpeg"
+            title="economic-development">
+            <Typography className={classes.title} gutterBottom variant="h5" component="h2">
+              Economic Development
+            </Typography>
+            <Button className={classes.button}>
+              <Link className={classes.link} to="/economic-development">Read More</Link>
+            </Button>
+          </CardMedia>
+        </Card>
+
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.media}
+            image="images/human-capitol.jpeg"
+            title="human-capitol">
+            <Typography className={classes.title} gutterBottom variant="h5" component="h2">
+              Human Capitol
+            </Typography>
+            <Button className={classes.button}>
+              <Link className={classes.link} to="/human-capitol">Read More</Link>
+            </Button>
+          </CardMedia>
+        </Card>
+
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.media}
+            image="images/access-transit.jpeg"
+            title="access-transit">
+            <Typography className={classes.title} gutterBottom variant="h5" component="h2">
+              Access & Transit
+            </Typography>
+            <Button className={classes.button}>
+              <Link className={classes.link} to="/access-transit">Read More</Link>
+            </Button>
+          </CardMedia>
+        </Card>
+      </div>
+
+      <div className="contact">
+        <span className="phone">(612) 351-8200</span> 
+        <span className="email">info@centerforeconomicinclusion.org</span>
+      </div>
+    </footer>
+  )
+};
+
+Footer.propTypes = {
+  classes: propTypes.object.isRequired,
+};
+
+export default (withStyles(styles)(Footer));
