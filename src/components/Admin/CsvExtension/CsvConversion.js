f902fcdb2b38f0647013ed33712c96a30ea4b47d
Cooper Miller <coopermllr@gmail.com>
2018-12-18 11:31:49 -0600

    updates to csv page

--- src/components/Admin/CsvExtension/CsvConversion.js
+++ src/components/Admin/CsvExtension/CsvConversion.js
@@ -3,19 +3,47 @@ import Button from '@material-ui/core/Button';
 
 class CsvConversion extends Component {
 
+    state = {
+        csv: '',
+    }
+
+   // handle changes in the form inputs
+    handleChange = event => {
+        console.log('handleChange', event.target.value)
+        this.setState({
+                ...this.state,
+                [event.target.name]: event.target.value,
+        });
+    }
+
+    // submit project information from form
+    addCsv = event => {
+        console.log('Adding CSV: ', this.state);
+        event.preventDefault();
+        // this.props.dispatch({ type: 'ADD_CSV', payload: this.state })
+        this.setState({
+            csv: '',
+        });
+    }
+
     render() {
         return (
             <div>
-                {JSON.stringify('hi')}
-                <Button id="submitCsv">
-                <input 
-                    id="myFile" 
-                    type="file" 
-                    onChange={this.addCsv}
-                    accept=".csv" 
-                />
-                </Button>
-                <br></br>
+                {JSON.stringify(this.state)}
+                <form onSubmit={this.addCsv}>
+                    <input 
+                        id="myFile" 
+                        type="file" 
+                        // accept=".csv" 
+                        value={this.state.csv} 
+                        name="csv"
+                        onChange={this.handleChange} 
+                    />
+                    <Button id="submitCsv" type="submit" variant="outlined" color="primary">
+                        Add CSV
+                    </Button>
+                </form>
+                <img src={this.state.csv} alt="info-here" />
             </div>
         );
     }
