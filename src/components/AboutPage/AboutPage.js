12a6e4acd20cbb80d2c0e641ee3d6ef7df31f711
Lex Chudzik <chudzik.alex@gmail.com>
2018-12-18 10:55:09 -0600

    test map working

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import tracts from './mnct';
+import Choropleth from 'react-leaflet-choropleth';
+import '../../../node_modules/leaflet/dist/leaflet.css';
+import './map.css'
+import { Map, Marker, Popup, TileLayer, GeoJSON } from 'react-leaflet';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
+const accessToken = process.env.MAPBOX_API_KEY;
+const stamenTonerTiles = `https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=${accessToken}`;
+const stamenTonerAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+const mapCenter = [44.972, -93.1663];
+const zoomLevel = 12;
 
-const AboutPage = () => (
-  <div>
-    <div>
-      <p>
-        This about page is for anyone to read!
-      </p>
-    </div>
-  </div>
-);
+class AboutPage extends Component {
+
+    render() {
+        return (
+            <div className="map">
+                <Map
+                    center={mapCenter}
+                    zoom={zoomLevel}
+                >
+                    <TileLayer
+                        attribution={stamenTonerAttr}
+                        url={stamenTonerTiles}
+                    />
+                        <Choropleth
+                      data={{type: 'FeatureCollection', features: tracts.features}}
+                      valueProperty={(feature) => feature.properties.ALAND}
+                      // visible={(feature) => feature.id !== active.id}
+                      scale={['#b3cde0', '#011f4b']}
+                      steps={7}
+                      mode='e'
+                      style={{
+                        fillColor: '#F28F3B',
+                        weight: 1,
+                        opacity: 0.5,
+                        color: 'white',
+                        dashArray: '3',
+                        fillOpacity: 0.8
+                    }}
+                      onEachFeature={(feature, layer) => layer.bindPopup(feature.properties.AWATER)}
+                      ref={(el) => this.choropleth = el.leafletElement}
+                    />
+                    {/* <GeoJSON data={tracts} style={this.getStyle} /> */}
+                </Map>
+            </div>
+        );
+    }
+}
 
 export default AboutPage;
--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
-
-const InfoPage = () => (
-  <div>
-    <p>
-      Info Page
-    </p>
-  </div>
-);
+class InfoPage extends React.Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
 
 export default InfoPage;
