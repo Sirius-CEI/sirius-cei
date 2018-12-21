1868a5a7e498428f0dcdfdbe4681ce78aff861a8
Lex Chudzik <chudzik.alex@gmail.com>
2018-12-21 12:30:26 -0600

    redoing map

--- src/components/IndicatorPage/MapIndicator.js
+++ src/components/IndicatorPage/MapIndicator.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 import tracts from './tracts';
-import Choropleth from 'react-leaflet-choropleth';
+//import Choropleth from 'react-leaflet-choropleth';
 import 'leaflet/dist/leaflet.css';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, GeoJSON, FeatureGroup } from 'react-leaflet';
 import './map.css';
+import MapLegend from './MapLegend'
 
-const accessToken = process.env.MAPBOX_API_KEY;
-const stamenTonerTiles = `https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=${process.env.MAPBOX_API_KEY}`;
+const accessToken = 'pk.eyJ1IjoibGV4Y2h1ZHppayIsImEiOiJjanBzaWx5dG8wdGppM3htaDhiZ3RwcXJ6In0.-gllVsZonwCccRMzb1DmYQ';
+const stamenTonerTiles = `https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=${accessToken}`;
 const stamenTonerAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
 const mapCenter = [44.972, -93.1663];
 const zoomLevel = 10;
 
 class MapIndicator extends Component {
+
+	color = (d) => {
+		return d > 1000000 ? '#800026' :
+		d > 500000  ? '#BD0026' :
+		d > 200000  ? '#E31A1C' :
+		d > 100000  ? '#FC4E2A' :
+		d > 50000   ? '#FD8D3C' :
+		d > 20000   ? '#FEB24C' :
+		d > 10000   ? '#FED976' :
+				   '#FFEDA0';
+	}
+
+	choropleth = (feature) => {
+		let color = this.color(feature.properties.ALAND)
+		return {
+			fillColor: color,
+			weight: 1,
+			opacity: 0.5,
+			color: 'white',
+			dashArray: '3',
+			fillOpacity: 0.5
+		}
+	}
 
 	render() {
         return (
@@ -25,7 +49,15 @@ class MapIndicator extends Component {
                         attribution={stamenTonerAttr}
                         url={stamenTonerTiles}
                     />
-                    <Choropleth
+					<FeatureGroup>
+						{tracts.features.map( (feature) =>
+							(<GeoJSON
+							data={feature}
+							style={this.choropleth(feature)}
+							/>)
+						) }
+					</FeatureGroup>
+                    {/* <Choropleth
                       data={{type: 'FeatureCollection', features: tracts.features}}
                       valueProperty={(feature) => feature.properties.ALAND}
                       // visible={(feature) => feature.id !== active.id}
@@ -42,7 +74,8 @@ class MapIndicator extends Component {
                     }}
                       //onEachFeature={(feature, layer) => layer.bindPopup(feature.properties.ALAND)}
                       //ref={(el) => this.choropleth = el.leafletElement}
-                    />
+                    /> */}
+					<MapLegend />
 
                 </Map>
             </div>
