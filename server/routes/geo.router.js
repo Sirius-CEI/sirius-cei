ee01340a531c7805ba4bfa1f54b129f63a85123f
KarlaJeanNelson <karla.jean.nelson@gmail.com>
2019-01-07 14:48:45 -0600

    cleaned up after loading geo data

--- server/routes/geo.router.js
+++ server/routes/geo.router.js
@@ -2,20 +2,6 @@ const express = require('express');
 const router = express.Router();
 const County = require('../models/counties.model');
 const Tracts = require('../models/tracts.model');
-const countyData = require('../data/counties-geo.json');
-const tractsData = require('../data/tracts-geo.json');
-
-router.post('/counties', (req, res) => {
-	County.insertMany(countyData)
-	.then((docs) => (res.json({ success: true, docs: docs.length })))
-	.catch((err) => (res.json({ success: false, error: err.message })))
-});
-
-router.post('/tracts', (req, res) => {
-	Tracts.insertMany(tractsData, { ordered: false })
-	.then((docs) => (res.json({ success: true, docs: docs.length })))
-	.catch((err) => (res.json({ success: false, error: err.message })))
-});
 
 router.get('/counties', (req, res) => {
 	County.find({})
