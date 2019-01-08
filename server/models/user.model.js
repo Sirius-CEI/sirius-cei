fbfc3bb2ef5b0bd0b90031112ac97a3509ee9602
Cooper Miller <coopermllr@gmail.com>
2019-01-08 10:26:36 -0600

    update to changes

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const uniqueValidator = require("mongoose-unique-validator");
-const bcrypt = require("bcrypt");
-
-const UserSchema = new Schema({
- username: { type: String, required: true, unique: true },
- passwordHash: { type: String, required: true }
-});
-
-UserSchema.plugin(uniqueValidator);
-
-UserSchema.methods.validPassword = function(password) {
- return bcrypt.compareSync(password, this.passwordHash);
-};
-
-UserSchema.virtual("password").set(function(value) {
- this.passwordHash = bcrypt.hashSync(value, 12);
-});
-
-const User = mongoose.model("User", UserSchema);
-
-module.exports = User;
-=======
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -36,4 +11,3 @@ const UserSchema = new Schema({
 });
 
 module.exports = mongoose.model('user', UserSchema);
->>>>>>> a13a45a1103a385f3065daef5f969e0d1e440c41
