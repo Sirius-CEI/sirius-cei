0ae5ec34e065141adf5ea9ba5c1f6d7649e7bd40
Lex Chudzik <chudzik.alex@gmail.com>
2019-01-08 14:41:57 -0600

    outcome header and indicator text displaying

--- src/views/IndicatorPage/TextIndicator.js
+++ src/views/IndicatorPage/TextIndicator.js
@@ -25,12 +25,12 @@ const styles = theme => ({
 
 class TextField extends Component {
   render() {
-    const { classes } = this.props;
+    const { classes, indicator } = this.props;
       return (
         <div className={classes.root}>
-          <Typography className={classes.indicatorTitle}>Wages</Typography>
+          <Typography className={classes.indicatorTitle}>{indicator.title}</Typography>
             <Typography className={classes.indicatorText}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Feugiat scelerisque varius morbi enim nunc faucibus a. Tincidunt augue interdum velit euismod. Tellus at urna condimentum mattis pellentesque. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Enim ut tellus elementum sagittis vitae et leo duis. Libero justo laoreet sit amet cursus. Vitae nunc sed velit dignissim sodales ut eu sem. Ut consequat semper viverra nam libero justo laoreet sit amet.
+              {indicator.copy}
             </Typography>
         </div>
       );
