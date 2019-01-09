45ea34a1c2701fee1e52608ad1e663627ef65ea4
Cooper Miller <coopermllr@gmail.com>
2019-01-09 15:16:50 -0600

    password reset added, needs style updates

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -163,7 +163,7 @@ router.post('/password-reset', (req, res, next) => {
           have requested the reset of the password for your account.\n\n` +
           `Please click on the following link, or paste this into 
           your browser to complete the process within one hour of receiving it:\n\n` +
-          `http://localhost:3000/#/reset/${token}/\n\n` +
+          `http://localhost:3000/reset/${token}/\n\n` +
           `If you did not request this, please ignore this email and 
           your password will remain unchanged.\n`,
       };
