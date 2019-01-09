ed097da731fa840bdd2540fae2788f7933e76f07
Cooper Miller <coopermllr@gmail.com>
2019-01-09 10:54:40 -0600

    mvp email password reset complete

--- server/routes/forgotPassword.router.js
+++ server/routes/forgotPassword.router.js
@@ -2,24 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Person = require('../models/user.model');
 require('dotenv').config();
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
 
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 
-//GET route to get a token from user
-router.get('/:id', (req, res) => {
-  console.log('req.params in forgot password GET', req.params);
-  Person.find({
-      resetPasswordToken: req.query,
-      resetPasswordExpires: {
-        [Op.gt]: Date.now(),
-      },
-    }
-  ).then(user => {
-    if (user == null) {
-      console.log('password reset link is invalid or has expired');
+//GET route to get a token from user and check if
+// the users reset password time is valid
+router.get('/:resetPasswordToken', (req, res) => {
+  console.log('req.params in forgot password GET', req.params.resetPasswordToken);
+  Person.findOne(
+    { resetPasswordToken: req.params.resetPasswordToken },
+    {},
+    { resetPasswordExpires: { $gt: Date.now(), }, 
+  }).then(user => {
+    console.log('user info: ', user);
+    if (user == undefined) {
+      console.log('password reset link is invalid or has expired', req.body.username);
       res.json('password reset link is invalid or has expired');
     } else {
       res.status(200).send({
@@ -27,68 +25,68 @@ router.get('/:id', (req, res) => {
         message: 'password reset link a-ok',
       });
     }
+  })
+  .catch((error) => {
+    console.log('error', error);
+    res.sendStatus(500)
   });
 });
 
-  router.post('/', (req, res, next) => {
-    console.log('in router.post forgot password', req.body.username);
-    if (req.body.username === '') {
-      res.json('email required');
+router.post('/', (req, res, next) => {
+  console.log('in router.post forgot password', req.body.username);
+  if (req.body.username === '') {
+    console.log('email required');
+  }
+  const token = crypto.randomBytes(20).toString('hex');
+  console.log(req.body.username);
+  Person.findOneAndUpdate(
+    { username: req.body.username },
+    { $set: { resetPasswordToken: token, resetPasswordExpires: Date.now() + 1800000 } },
+    (error, doc) => ( error ? res.json({ success: false, error: error.message }) : res.json({ success: true, doc: doc }))
+  ).then(user => {
+    console.log('users here ', user);
+    if (user === null) {
+      console.log('email not in database');
+      res.json('email not in db');
+    } else {
+      console.log('post users update', user);
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_ADDRESS,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+      const mailOptions = {
+        from: `ceimailtestmn@gmail.com`,
+        to: `${req.body.username}`,
+        subject: `Link To Reset Password`,
+        text:
+          `You are receiving this because you (or someone else) 
+          have requested the reset of the password for your account.\n\n` +
+          `Please click on the following link, or paste this into 
+          your browser to complete the process within one hour of receiving it:\n\n` +
+          `http://localhost:3000/#/reset/${token}/\n\n` +
+          `If you did not request this, please ignore this email and 
+          your password will remain unchanged.\n`,
+      };
+
+      console.log('sending mail');
+
+      transporter.sendMail(mailOptions, function(err, response) {
+        if (err) {
+          console.error('there was an error: ', err);
+        } else {
+          console.log('here is the res: ', response);
+          res.status(200).json('recovery email sent');
+        }
+      });
     }
-    const token = crypto.randomBytes(20).toString('hex');
-    console.log(req.body.username);
-    Person.findOneAndUpdate(
-      {
-        username: req.body.username,
-      },
-      { $set: { resetPasswordToken: token, resetPasswordExpires: Date.now() + 3600000 } },
-      (error, doc) => ( error ? res.json({ success: false, error: error.message }) : res.json({ success: true, doc: doc }))
-    ).then(user => {
-      console.log('users here ', user);
-      if (user === null) {
-        console.log('email not in database');
-        res.json('email not in db');
-      } else {
-        const token = crypto.randomBytes(20).toString('hex');
-        user.update(
-          { resetPasswordToken : token },
-          { resetPasswordExpires : Date.now() + 3600000 },
-        );
-        console.log('post users update', user);
-        const transporter = nodemailer.createTransport({
-          service: 'gmail',
-          auth: {
-            user: process.env.EMAIL_ADDRESS,
-            pass: process.env.EMAIL_PASSWORD,
-          },
-        });
-
-        const mailOptions = {
-          from: `ceimailtestmn@gmail.com`,
-          to: `${req.body.username}`,
-          subject: `Link To Reset Password`,
-          text:
-            `You are receiving this because you (or someone else) 
-            have requested the reset of the password for your account.\n\n` +
-            `Please click on the following link, or paste this into 
-            your browser to complete the process within one hour of receiving it:\n\n` +
-            `http://localhost:3000/#/reset/${token}/\n\n` +
-            `If you did not request this, please ignore this email and 
-            your password will remain unchanged.\n`,
-        };
-
-        console.log('sending mail');
-
-        transporter.sendMail(mailOptions, function(err, response) {
-          if (err) {
-            console.error('there was an error: ', err);
-          } else {
-            console.log('here is the res: ', response);
-            res.status(200).json('recovery email sent');
-          }
-        });
-      }
-    });
-  });
+  })
+  .catch( (error) => {
+    console.log('error', error);
+    res.sendStatus(500);
+  })
+});
 
 module.exports = router;
