13b320272ac7ba3e5675685644de7088618af1f0
Lex Chudzik <chudzik.alex@gmail.com>
2019-01-09 10:10:55 -0600

    switched chart to google charts

--- src/views/IndicatorPage/GraphIndicator.js
+++ src/views/IndicatorPage/GraphIndicator.js
@@ -6,7 +6,7 @@ import propTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
-import { Line } from 'react-chartjs-2';
+import Chart from 'react-google-charts';
 
 const styles = theme => ({
   button: {
@@ -26,19 +26,14 @@ const styles = theme => ({
   } 
 });
 
-let newData = [40,50,60,60,80,90];
-
-const data = {
-  labels: ['2014', '2015', '2016', '2017', '2018'],
-  datasets: [{
-      data: newData,
-      fill: false,
-    }],
-    animation: {
-        duration: 1000,
-        easing: 'linear'
-    }
-};
+const data = [
+  ['year', 'one', 'two', 'three', 'four', 'five', 'six'],
+  ['2013', 5, 6, 3, 2, 2, 5],
+  ['2014', 3, 4, 3, 2, 4, 4],
+  ['2015', 4, 4, 5, 2, 6, 4.5], 
+  ['2016', 6, 7, 4, 2, 1, 2],
+  ['2017', 3, 5, 6, 3, 8, 7]
+];
 
 class GraphIndicator extends Component {
 
@@ -57,19 +52,24 @@ class GraphIndicator extends Component {
   render() {
     const { classes } = this.props;
       return (
-        <div className={classes.root}>
+        <div>
+        <div>
           {
             this.state.display === 'location' ? (
               <MapIndicator/>
             ) : (
-              <Line
-              data={data}
+              <Chart
+                width={'100%'}
+                height={'100%'}
+                chartType="Line"
+                data={data}
                 options={{
-                title: false,
-                maintainAspectRatio: false
-              }}/>
+                  colors: ['#4c2a74', '#008ab7', '#02c39a', '#ffc100', '#ff784f', '#d0021b']
+                }}
+              />
             )
           }
+          </div>
           <div className={classes.button}>
             <ToggleButtonGroup exclusive value={this.state.display} onChange={this.handleDisplay}>
               <ToggleButton value="race">Race/Ethnicity</ToggleButton>
