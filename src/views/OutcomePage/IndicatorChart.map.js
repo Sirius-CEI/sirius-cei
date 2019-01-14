23c82e8c5ba31f5b8c68aae0c46f5577520d545d
Lex Chudzik <chudzik.alex@gmail.com>
2019-01-14 11:26:22 -0600

    map working

--- src/views/OutcomePage/IndicatorChart.map.js
+++ src/views/OutcomePage/IndicatorChart.map.js
@@ -1,45 +1,69 @@
 import React, { Component } from 'react';
 import tracts from './tracts';
-//import Choropleth from 'react-leaflet-choropleth';
+import counties from './counties';
 import 'leaflet/dist/leaflet.css';
+import { connect } from 'react-redux';
 import { Map, TileLayer, GeoJSON, FeatureGroup } from 'react-leaflet';
 import './map.css';
-import MapLegend from './IndicatorChart.map.legend'
+// import MapLegend from './IndicatorChart.map.legend'
 import chroma from 'chroma-js';
 
 const accessToken = 'pk.eyJ1IjoibGV4Y2h1ZHppayIsImEiOiJjanBzaWx5dG8wdGppM3htaDhiZ3RwcXJ6In0.-gllVsZonwCccRMzb1DmYQ';
 const stamenTonerTiles = `https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=${accessToken}`;
-const stamenTonerAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
-const mapCenter = [44.972, -93.1663];
-const zoomLevel = 8;
+const stamenTonerAttr = 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+const mapCenter = [45.2, -93.43];
+const zoomLevel = 7;
 
 
 class MapIndicator extends Component {
 
     state = {
-        grades: [0, 10000, 20000, 50000, 100000, 200000, 500000, 1000000],
-        labels: []
+        grades: [],
+        labels: [],
+        data: []
     }
 
 	color = (d) => {
-		return d > 1000000 ? '#810f7c' :
-		d > 500000  ? '#8856a7' :
-		d > 200000  ? '#8c96c6' :
-		d > 100000  ? '#b3cde3' :
+		return d > this.state.grades[3] ? '#88419d' :
+		d > this.state.grades[2] ? '#8c96c6' :
+		d > this.state.grades[1] ? '#b3cde3' :
 				   '#edf8fb';
-	}
+    }
+    
+    componentWillMount = () => {
+        let data = this.props.chartData.filter(item => (item.chart === this.props.chart._id));
+        let values = data.map((item) => item.value);
+        let grades = chroma.limits(values, 'e', 4);
+        this.setState({
+            ...this.state,
+            grades: grades,
+            data: data,
+        })
+        console.log(grades);
+    }
 
 	choropleth = (feature) => {
-        this.setState.grades = chroma.limits([1,3,4], )
-		let color = this.color(feature.properties.ALAND)
-		return {
-			fillColor: color,
-			weight: 1,
-			opacity: 0.5,
-			color: 'white',
-			dashArray: '3',
-			fillOpacity: 0.5
-		}
+        let id = feature.properties.GEOID;
+        let dataPoint = this.state.data.find((x) => {return x.variable === id})
+        console.log(dataPoint);
+        if (dataPoint) {
+            let color = this.color(dataPoint.value)
+            return {
+                fillColor: color,
+                weight: 1,
+                opacity: 0.8,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 0.5
+            }
+        } else {return {
+            fillColor: 'grey',
+            weight: 1,
+            opacity: 0.8,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.5
+        }}
 	}
 
 	render() {
@@ -55,33 +79,15 @@ class MapIndicator extends Component {
                         url={stamenTonerTiles}
                     />
 					<FeatureGroup>
-						{tracts.features.map( (feature) =>
+						{counties.features.map( (feature) =>
 							(<GeoJSON
-                            key={feature.id}
+                            key={feature._id.$oid}
 							data={feature}
 							style={this.choropleth(feature)}
 							/>)
 						) }
 					</FeatureGroup>
-                    {/* <Choropleth
-                      data={{type: 'FeatureCollection', features: tracts.features}}
-                      valueProperty={(feature) => feature.properties.ALAND}
-                      // visible={(feature) => feature.id !== active.id}
-                      scale={['#ffffb2', '#bd0026']}
-                      steps={5}
-                      mode='k'
-                      style={{
-                        fillColor: '#F28F3B',
-                        weight: 1,
-                        opacity: 0.5,
-                        color: 'white',
-                        dashArray: '3',
-                        fillOpacity: 0.5
-                    }}
-                      //onEachFeature={(feature, layer) => layer.bindPopup(feature.properties.ALAND)}
-                      //ref={(el) => this.choropleth = el.leafletElement}
-                    /> */}
-					<MapLegend />
+					{/* <MapLegend /> */}
 
                 </Map>
             </div>
@@ -89,4 +95,8 @@ class MapIndicator extends Component {
     }
 }
 
-export default MapIndicator;
+const mapStateToProps = state => ({
+	chartData: state.chartData,
+});
+
+export default connect(mapStateToProps)(MapIndicator);
